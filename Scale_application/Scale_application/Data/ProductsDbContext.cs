b63fb290c77e4b8bcd7f99a1514a5e8ea040095e
Nurji Ibrayev <30404638+nurji-ibrayev@users.noreply.github.com>
2023-05-16 18:14:07 +0600

    Add files via upload

---
+++ Scale_application/Scale_application/Data/ProductsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scale_application.Models.Entities;
+
+namespace Scale_application.Data
+{
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
