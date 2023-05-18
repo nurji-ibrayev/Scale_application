7344c6899bc7eacdc57d7a9476847e48d345ae5d
Nurji Ibrayev <30404638+nurji-ibrayev@users.noreply.github.com>
2023-05-18 12:55:49 +0600

    Add files via upload

---
+++ Scale_application/Scale_application/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scale_application.Data;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace Scale_application
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<CategoriesDbContext>();
+                context.Database.Migrate();
+            }
+        }
+    }
+}
