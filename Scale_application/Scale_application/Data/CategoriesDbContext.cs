00c2cb5358e10b591dafbafb57b967782d9fb504
Nurji Ibrayev <30404638+nurji-ibrayev@users.noreply.github.com>
2023-05-18 12:57:11 +0600

    Add files via upload

---
+++ Scale_application/Scale_application/Data/CategoriesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scale_application.Models.Entities;
+
+namespace Scale_application.Data
+{
+    public class CategoriesDbContext : DbContext
+    {
+        public CategoriesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
